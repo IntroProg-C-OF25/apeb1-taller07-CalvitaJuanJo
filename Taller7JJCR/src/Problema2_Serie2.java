088336a0804896371452693abe671ec896bf38a1
CalvitaJuanJo <juanjosecalva@gmail.com>
2024-11-21 12:16:35 -0500

    Ejercicios realizados

---
+++ Taller7JJCR/src/Problema2_Serie2.java
@@ -0,0 +1,27 @@
+/***
+ * Problema 2
+Realizar un programa Java que permita presentar en pantalla la siguiente 
+* secuencia: 5/10, 10/12, 15/14, 20/16, 25/18, 30/20
+ */
+import java.util.Scanner;
+
+public class Problema2_Serie2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+       int contador = 5, limite;
+        System.out.print("Dame limite: ");
+        limite = teclado.nextInt();
+        while (contador <= limite) {
+            System.out.print(contador + "/" + ((contador / 5) * 2 + 8) + ", ");
+            contador += 5;
+        }
+        System.out.println("");
+    }
+}
+/***
+ * run:
+ * Dame limite: 30
+ * 5/10, 10/12, 15/14, 20/16, 25/18, 30/20, 
+ * BUILD SUCCESSFUL (total time: 1 second)
+
+ */
---
+++ Taller7JJCR/src/Problema5_Promedios.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+/***
+ * Problema 5
+ * Generar un programa Java que permita ingresar 4 estudiantes; por cada uno de 
+ * ellos ingresar el nombre del estudiante, el promedio de ciclo. Presentar el 
+ * siguiente reporte
+ * Estudiante1	10	Aprobado
+ * Estudiante2	6.9	Reprobado
+ * Estudiante3	7	Aprobado
+ * Estudiante4	5	Reprobado
+ * Atención; con base al valor del promedio, usted debe asignar en cada registro 
+ * el tipo Aprobado o Reprobado.
+ * @author UTPL
+ */
+public class Problema5_Promedios {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String nombre, estado;
+        double promedio;
+        int contador = 1;
+        while (contador <= 4) {
+            System.out.print("Dame nombre del estudiante y su promedio del EST_" + contador + ": ");
+            nombre = teclado.next();
+            promedio = teclado.nextDouble();
+            if (promedio < 6.5)
+                estado = "Reprobado";
+            else
+                estado = "Aprobado";
+            System.out.printf("|%s\t|%s | %s|\n","nombre", "promedio", "estado");
+            System.out.printf("| %s\t | %.2f | %s\t|\n", nombre, promedio, estado);
+            contador ++;
+        }
+    }
+}
+/***
+ * run:
+ * Dame nombre del estudiante y su promedio del EST_1: juan 5 
+ * |nombre |promedio | estado|
+ * | juan  | 5,00    | Reprobado	
+ * Dame nombre del estudiante y su promedio del EST_2: pedro 5
+ * |nombre | promedio | estado|
+ * | pedro |   5,00   | Reprobado	| 
+ * Dame nombre del estudiante y su promedio del EST_3: angel 3
+ * |nombre |promedio | estado|
+ * | angel | 3,00    | Reprobado	|
+ * Dame nombre del estudiante y su promedio del EST_4: chofa 1
+ * |nombre |promedio | estado|
+ * | chofa | 1,00    | Reprobado	|
+ * BUILD SUCCESSFUL (total time: 48 seconds)
+ */
